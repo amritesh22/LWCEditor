7438affbb9930f69efeeb3dd29c785e1dc818b1f
Amritesh <77649031+amritesh22@users.noreply.github.com>
2023-09-10 08:23:16 +0100

    update

--- force-app/main/default/lwc/lwcEditorLauncher/lwcEditorLauncher.js
+++ force-app/main/default/lwc/lwcEditorLauncher/lwcEditorLauncher.js
@@ -4,7 +4,6 @@ import { NavigationMixin } from 'lightning/navigation';
 export default class lwcEditorLauncher extends NavigationMixin(LightningElement) {
 
     openLwcEditor() {
-        //window.open('/c/LwcEditorApp.app', '_blank');
         const url = '/c/LwcEditorApp.app';
         this[NavigationMixin.Navigate]({
             type: 'standard__webPage',
